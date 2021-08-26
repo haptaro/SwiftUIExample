f8d3988953a90b2221dfc208472d95a69d3df8e9
Kotaro Fukuo <haptaro.dev@gmail.com>
2021-08-26 14:07:42 -0700

    :art: add Sheet that has condition of interaction

--- SwiftUIExample/ImageMoreDetail.swift
+++ SwiftUIExample/ImageMoreDetail.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ImageMoreDetail: View {
   @Binding var string: String
+  @State private var showSheet = false
 
   var body: some View {
     VStack {
@@ -20,6 +21,34 @@ struct ImageMoreDetail: View {
         .textFieldStyle(.roundedBorder)
         .font(.title3)
         .padding([.top, .leading, .trailing], 30)
+      InputTextFromSheet()
+    }
+  }
+}
+
+private extension ImageMoreDetail {
+
+  struct InputTextFromSheet: View {
+    @State private var inputText = ""
+    @State private var showInputSheet = false
+
+    var body: some View {
+      VStack(spacing: 30) {
+        Text("Input from sheet is \(inputText)")
+        Button(action: {
+          showInputSheet.toggle()
+        }, label: {
+          Text("Open sheet")
+        })
+          .sheet(isPresented: $showInputSheet) {
+            TextField("Placeholder", text: $inputText)
+              .textFieldStyle(.roundedBorder)
+              .interactiveDismissDisabled(inputText.isEmpty)
+          }
+      }
+      .padding(30)
+      .background(Color.gray)
+      .cornerRadius(25)
     }
   }
 }
